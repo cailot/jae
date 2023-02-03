61ce0463ff10ddda86be36937b29618aa97dad10
cailot <jh05052008@gmail.com>
2023-02-03 04:06:45 +0000

    add more into Student Model

--- src/main/java/hyung/jin/seo/jae/model/Student.java
+++ src/main/java/hyung/jin/seo/jae/model/Student.java
@@ -4,6 +4,9 @@ import java.io.Serializable;
 import org.apache.commons.lang3.StringUtils;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -17,7 +20,13 @@ import javax.persistence.Id;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Column;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.Date;
+
 
 @Getter
 @Setter
@@ -38,10 +47,37 @@ public class Student implements Serializable{
     @Column(length = 200, nullable = false)
     public String lastName;
     
+    @Column(length = 10, nullable = false)
+    public String grade;
+    
+    @Column(length = 200, nullable = false)
+    public String contactNo1;
+    
+    @Column(length = 200, nullable = false)
+    public String contactNo2;
+    
+    @Column(length = 300, nullable = false)
+    public String email;
+    
+    @Column(length = 500, nullable = false)
+    public String address;
+    
+    @Column(length = 3, nullable = false)
+    public String state;
+    
+    @Column(length = 50, nullable = false)
+    public String branch;
+    
+    @Column(length = 1000, nullable = false)
+    public String memo;
+    
     @CreationTimestamp
-    public LocalDateTime dateCreated;
+    public LocalDate registerDate;
     
     @UpdateTimestamp
-    public LocalDateTime dateUpdated;
+    public LocalDate enrolmentDate;
+    
+    @CreatedDate
+    public LocalDate endDate;
     
 }
