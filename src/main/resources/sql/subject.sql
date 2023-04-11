d49c6ec2e99bce1aaca309953bdebfdb88110a0c
Jinhyung Seo <jin@flutter.com>
2023-04-11 16:52:48 +1000

    braybrook studendt sql

---
+++ src/main/resources/sql/subject.sql
@@ -0,0 +1,7 @@
+INSERT INTO jae.subject(abr,description,name,register_date) VALUES ('ENG','English Subject','English',sysdate());
+INSERT INTO jae.subject(abr,description,name,register_date) VALUES ('MAT','Maths Subject','Maths',sysdate());
+INSERT INTO jae.subject(abr,description,name,register_date) VALUES ('GA','General Ability Subject','General Ability',sysdate());
+INSERT INTO jae.subject(abr,description,name,register_date) VALUES ('W','Writing Subject','Writing',sysdate());
+INSERT INTO jae.subject(abr,description,name,register_date) VALUES ('SC','Science Subject','Science',sysdate());
+INSERT INTO jae.subject(abr,description,name,register_date) VALUES ('ALL','VCE Subject','All',sysdate());
+commit;
