31e60cc59b165fe3833cddddc8c76d999165788f
Jinhyung Seo <jin@good.com>
2023-04-18 23:06:58 +1000

    automate running scripts when launching

---
+++ src/main/java/hyung/jin/seo/jae/SqlScriptRunner.java
@@ -0,0 +1,35 @@
+package hyung.jin.seo.jae;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
+import org.springframework.stereotype.Component;
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@Component
+public class SqlScriptRunner {
+
+    @Autowired
+    private ResourceLoader resourceLoader;
+
+    @Autowired
+    private DataSource dataSource;
+    
+    @Autowired
+    private Environment env;
+
+    @PostConstruct
+    public void runSqlScripts() throws SQLException {
+    	String ddl = env.getProperty("spring.jpa.hibernate.ddl-auto");
+    	// run scripts only for 'create-drop'
+    	if("create-drop".equalsIgnoreCase(ddl)) { 
+	        Connection connection = dataSource.getConnection();
+	        ScriptUtils.executeSqlScript(connection, resourceLoader.getResource("classpath:sql/elearning.sql"));
+	        ScriptUtils.executeSqlScript(connection, resourceLoader.getResource("classpath:sql/braybrook_student.sql"));
+    	}
+    }
+}
---
+++ src/main/resources/sql/course_fee.sql
@@ -0,0 +1,1 @@
+INSERT INTO jae.CourseFee (name, grade, year, price, registerDate) VALUES ('3 Subjects','p2', '2022', 67, sysdate());
