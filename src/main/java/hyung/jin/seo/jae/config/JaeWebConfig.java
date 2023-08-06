3d647783852b8ae3da4df3218e4333e52fd07535
Jin <jin@macbookk2023.com>
2023-08-06 13:54:22 +0900

    update CORS info - only for development

--- src/main/java/hyung/jin/seo/jae/config/JaeWebConfig.java
+++ src/main/java/hyung/jin/seo/jae/config/JaeWebConfig.java
@@ -18,12 +18,23 @@ public class JaeWebConfig implements WebMvcConfigurer {
 		.setCacheControl(CacheControl.maxAge(2, TimeUnit.HOURS).cachePublic());
 	}
 
+	// @Override
+	// public void addCorsMappings(CorsRegistry registry) {
+	// 	 registry.addMapping("/**")
+    //             .allowedOrigins("http://localhost:8080") // Add the allowed origin here
+    //             .allowedMethods("GET", "POST", "PUT", "DELETE") // Add the allowed HTTP methods here
+    //             .allowedHeaders("*"); // Add the allowed headers here
+	// }
+
+
 	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		 registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080") // Add the allowed origin here
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // Add the allowed HTTP methods here
-                .allowedHeaders("*"); // Add the allowed headers here
-	}
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("*")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+            .maxAge(3600);
+    }
 
 }
